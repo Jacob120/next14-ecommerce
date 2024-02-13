57082379c3b30eb733cb8dcec60ad59eda66c979
Jakub Kozik <jakubkozik120@gmail.com>
2024-02-13 21:24:01 +0100

    Fix lint errors

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,9 +1,5 @@
-import { Suspense } from "react";
 import { type Metadata } from "next/types";
 import { getProductById } from "@/api/products";
-import { ProductDescription } from "@/components/atoms/ProductDescription";
-import { ProductImage } from "@/components/atoms/ProductImage";
-import { SuggestedProductsList } from "@/components/organisms/SuggestedProducts";
 
 export const generateMetadata = async ({
 	params,
